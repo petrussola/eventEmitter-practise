ead197026f3f2e2feb6ed272c4ac4da674818372
Pere <pere.solaclaver@gmail.com>
2020-08-30 23:07:33 +0100

    Some changes to my own event emitter

--- my_own_event_emitter.js
+++ my_own_event_emitter.js
@@ -8,7 +8,7 @@ class EventEmitter {
 		this.init();
 	}
 
-	on(name, listener) {
+	addListener(name, listener) {
 		// if event is not listed yet, we initialise it as an array that will store the listeners
 		if (!this.events[name]) {
 			this.events[name] = [];
@@ -21,6 +21,11 @@ class EventEmitter {
 		console.log(this.events);
 	}
 
+	on(name, listener) {
+		// on is the same as addListener
+		return this.addListener(name, listener);
+	}
+
 	removeListener(name, listenerToRemove) {
 		if (!this.events[name]) {
 			throw new Error(`Can't remove listener, event ${name} doesn't exist`);
@@ -31,22 +36,52 @@ class EventEmitter {
 		});
 	}
 
-	emit(name, data) {
+	off(name, listenerToRemove) {
+		// off is same as remove listener
+		return this.removeListener(name, listenerToRemove);
+	}
+
+	once(name, listener) {
+		if (!this.events[name]) {
+			this.events[name] = [];
+		}
+
+		const onceWrapper = (...arg) => {
+			listener(...arg);
+			this.removeListener(name, onceWrapper);
+		};
+		this.events[name].push(onceWrapper);
+	}
+
+	emit(name, ...data) {
 		if (!this.events[name]) {
 			throw new Error(`Can't emit an event. Event ${name} does not exist.`);
 		}
 
 		this.events[name].forEach((cb) => {
-			cb(data);
+			cb(...data);
 		});
+	}
+
+	listenerCount(name) {
+		if (!this.events[name]) {
+			this.events[name] = [];
+		}
+		console.log(this.events[name].length);
+		return this.events[name].length;
+	}
+
+	rawListeners(name) {
+		return this.listeners[name];
 	}
 }
 
 // use EventEmitter
 const myEmitter = new EventEmitter();
 
-const handler1 = (arg) => {
-	console.log('Was fired: ', arg);
+const handler1 = (...arg) => {
+	console.log(...arg, '<<');
+	console.log('Was fired: ', ...arg);
 };
 
 myEmitter.on('testEvent', handler1);
@@ -55,3 +90,10 @@ myEmitter.on('testEvent2', handler1);
 myEmitter.emit('testEvent', 'hey there');
 myEmitter.emit('testEvent', 'firing event again');
 myEmitter.emit('testEvent', 'and again');
+
+myEmitter.on('testEvent3', handler1);
+myEmitter.listenerCount('testEvent3');
+console.log(myEmitter.events);
+myEmitter.emit('testEvent3', 'event3', 'asd');
+myEmitter.listenerCount('testEvent3');
+console.log(myEmitter.events);
